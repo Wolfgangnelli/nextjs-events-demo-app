0b3b01a2903ae89fb1462b34c3021d5964d40b63
[AndreaS] <solfa_90@hotmail.it>
2022-11-06 14:56:19 +0100

    add dummy data & funcs

---
+++ utils/index.ts
@@ -0,0 +1,26 @@
+import { DUMMY_EVENTS } from "../dummy-data";
+import { DateFilter } from "./types";
+
+export function getFeaturedEvents() {
+  return DUMMY_EVENTS.filter((event) => event.isFeatured);
+}
+
+export function getAllEvents() {
+  return DUMMY_EVENTS;
+}
+
+export function getFilteredEvents(dateFilter: DateFilter) {
+  const { year, month } = dateFilter;
+
+  let filteredEvents = DUMMY_EVENTS.filter((event) => {
+    const eventDate = new Date(event.date);
+    return (
+      eventDate.getFullYear() === year && eventDate.getMonth() === month - 1
+    );
+  });
+  return filteredEvents;
+}
+
+export function getEventById(id: string) {
+  return DUMMY_EVENTS.find((event) => event.id === id);
+}
