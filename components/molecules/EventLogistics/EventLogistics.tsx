056e07b651247ef459ff72a7f9b12e0376bea99a
[AndreaS] <solfa_90@hotmail.it>
2022-11-13 16:32:01 +0100

    fix sintax err

--- components/molecules/EventLogistics/EventLogistics.tsx
+++ components/molecules/EventLogistics/EventLogistics.tsx
@@ -1,25 +1,25 @@
-import React from 'react'
-import { Calendar, SImage } from '../../atoms'
-import { humanReadableDate, formattedAddress } from '../../../utils'
-import styles from './EventLogistics.module.sass'
+import React from "react";
+import { Calendar, SImage } from "../../atoms";
+import { humanReadableDate, formattedAddress } from "../../../utils";
+import styles from "./EventLogistics.module.sass";
 
 interface Props {
-    date: string
-    address: string
-    image: string
-    imageAlt: string
+  date: string;
+  address: string;
+  image: string;
+  imageAlt: string;
 }
 
 const EventLogistics = (props: Props) => {
-    const { date = '', address = '', image = '', imageAlt = '' } = props
+  const { date = "", address = "", image = "", imageAlt = "" } = props;
 
   return (
     <section>
-        <div>
-            <SImage src={image} alt={imageAlt} />
-        </div>
-    </section>>
-  )
-}
+      <div>
+        <SImage src={image} alt={imageAlt} />
+      </div>
+    </section>
+  );
+};
 
-export default EventLogistics
+export default EventLogistics;
