dd11b9d97f4c345b25a5c68251fa8b8a7e8f6e5f
[AndreaS] <solfa_90@hotmail.it>
2022-11-19 20:12:16 +0100

    navigate to filtered events page programmatically

--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -1,10 +1,21 @@
 import React from "react";
+import { useRouter } from "next/router";
 import { getAllEvents } from "../../utils";
 import { DummyEventType } from "../../utils/types";
 import { ListingEventiTemplate } from "../../components/templates";
+import { YEARS, MONTHS } from "../../dummy-data";
+import { FormSearchEvents } from "../../components/molecules";
 
 const EventsPage = () => {
   const events: DummyEventType[] = getAllEvents();
+
+  const router = useRouter();
+
+  const onSearch = (year: string, month: string) => {
+    const fullPath = `/events/${year}/${month}`;
+
+    router.push(fullPath);
+  };
 
   if (!events.length) {
     return <p>No events founds!</p>;
@@ -13,6 +24,7 @@ const EventsPage = () => {
   return (
     <div>
       <h1>The Events Page</h1>
+      <FormSearchEvents months={MONTHS} years={YEARS} onSearch={onSearch} />
       <div className="events-container">
         <ListingEventiTemplate events={events} />
       </div>
