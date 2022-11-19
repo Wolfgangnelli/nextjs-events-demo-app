46044dcb865571a3eca60a5bfa239229a4746528
[AndreaS] <solfa_90@hotmail.it>
2022-11-19 20:09:04 +0100

    bootstrap, fake data

--- dummy-data.js
+++ dummy-data.js
@@ -38,3 +38,29 @@ export const NAV_ITEMS = [
     href: "/events",
   },
 ];
+
+export const MONTHS = [
+  { label: "January", value: "01" },
+  { label: "February", value: "02" },
+  { label: "March", value: "03" },
+  { label: "April", value: "04" },
+  { label: "May", value: "05" },
+  { label: "June", value: "06" },
+  { label: "July", value: "07" },
+  { label: "August", value: "08" },
+  { label: "September", value: "09" },
+  { label: "October", value: "10" },
+  { label: "November", value: "11" },
+  { label: "December", value: "12" },
+];
+
+function getYears(startYear, total) {
+  let years = [];
+
+  for (let index = 0; index < total + 1; index++) {
+    years.push(startYear + index);
+  }
+  return years;
+}
+
+export const YEARS = getYears(1970, new Date().getFullYear() - 1970);
