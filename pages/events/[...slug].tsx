acc08f4829f0fabb08aa4d6f5ba6cc0343152721
Andrea Solfanelli <andrea.solfanelli@webranking.it>
2022-11-23 10:55:49 +0100

    extracting data catch-all filtered events page

--- pages/events/[...slug].tsx
+++ pages/events/[...slug].tsx
@@ -1,9 +1,41 @@
 import React from "react";
+import { useRouter } from 'next/router'
+import { getFilteredEvents } from '../../utils'
 
 const FilteredEventsPage = () => {
+
+  const { query } = useRouter()
+
+  const slugData = query.slug;
+
+  if(!slugData) {
+    return <p className="center">Loading...</p>
+  }
+
+  const numYear = +slugData[0];
+  const numMonth = +slugData[1];
+
+  if(isNaN(numYear) || isNaN(numMonth) || numYear > 2030 || numYear < 1970 || numMonth < 1 || numMonth > 12) {
+    return <p>Invalid filter. Please adjust your values!</p>
+  }
+
+  const filteredData = getFilteredEvents({
+    year: numYear,
+    month: numMonth,
+  })
+
+  if(!filteredData || filteredData.length === 0) {
+    return <p>No events found for the chosen filter!</p>
+  }
+
   return (
     <div>
       <h1>The Filtered Events Page</h1>
+        <div>
+          {filteredData.map(event => (
+            <p key={event.id}>{event.title}</p>
+          ))}
+        </div>
     </div>
   );
 };
