bac94da3fe847fbe09f674f248c35a5a2c94e01f
Andrea Solfanelli <andrea.solfanelli@webranking.it>
2022-12-02 16:30:16 +0100

    SSR

--- pages/events/[...slug].tsx
+++ pages/events/[...slug].tsx
@@ -1,24 +1,24 @@
 import React from "react";
 import { useRouter } from 'next/router'
+import { GetServerSidePropsContext } from 'next'
 import { getFilteredEvents } from '../../utils'
+import { RedirectPropsType, EventType, DatePropsType } from '../../utils/types'
 import { ListingEventiTemplate } from '../../components/templates'
 import { ResultsTitle } from '../../components/molecules'
 import { Button, AlertMessage } from '../../components/atoms'
 
-const FilteredEventsPage = () => {
+interface Props {
+  events: EventType[] | []
+  redirect?: RedirectPropsType
+  notFound?: boolean
+  hasError?: boolean
+  date?: DatePropsType
+}
 
-  const { query } = useRouter()
+const FilteredEventsPage = (props: Props) => {
+  const { events = [], hasError = false, date = undefined } = props
 
-  const slugData = query.slug;
-
-  if(!slugData) {
-    return <p className="center">Loading...</p>
-  }
-
-  const numYear = +slugData[0];
-  const numMonth = +slugData[1];
-
-  if(isNaN(numYear) || isNaN(numMonth) || numYear > 2030 || numYear < 1970 || numMonth < 1 || numMonth > 12) {
+  if(hasError) {
     return (
     <>
       <AlertMessage message="Invalid filter. Please adjust your values!" variant="info" />
@@ -29,30 +29,63 @@ const FilteredEventsPage = () => {
     )
   }
 
-  const filteredData = getFilteredEvents({
-    year: numYear,
-    month: numMonth,
-  })
-
-  if(!filteredData || filteredData.length === 0) {
+  if(!events || events.length === 0) {
     return (
       <>
-      <AlertMessage message="No events found for the chosen filter!" variant="info" />
-      <div className="center">
-      <Button label="Show All events" href="/events" />
-      </div>
+        <AlertMessage message="No events found for the chosen filter!" variant="info" />
+        <div className="center">
+          <Button label="Show All events" href="/events" />
+        </div>
       </>
     )
   }
 
-  const date = new Date(numYear, numMonth - 1)
+  const formatDate = date && new Date(date?.year, date?.month - 1)
 
   return (
     <>
-      <ResultsTitle date={date} />
-      <ListingEventiTemplate events={filteredData} />
+    {formatDate && ( <ResultsTitle date={formatDate} />)}
+      <ListingEventiTemplate events={events} />
     </>
   );
 };
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const { params } = context
+
+  const filteredData = params?.slug;
+
+  const filteredYear = filteredData?.[0]
+  const filteredMonth = filteredData?.[1]
+
+  const numYear = typeof filteredYear !== 'undefined' ? +filteredYear : 0
+  const numMonth = typeof filteredMonth !== 'undefined' ? +filteredMonth : 0
+
+  // check if the validation fail
+  if(isNaN(numYear) || isNaN(numMonth) || numYear > 2030 || numYear < 1970 || numMonth < 1 || numMonth > 12) {
+    return {
+/*         notFound: true, // show 404 page
+        redirect: {
+          destination: '/error' // redirect to a specific error page
+        }, */
+        props: {
+          hasError: true // pass a props that let me show a error message on this page
+        } 
+    }
+  }
+  
+  const filteredEvents = await getFilteredEvents({ year: numYear, month: numMonth })
+  
+  return {
+    props: {
+      events: filteredEvents,
+      date: {
+        year: numYear,
+        month: numMonth
+      }
+    }
+  }
+
+}
 
 export default FilteredEventsPage;
--- utils/index.ts
+++ utils/index.ts
@@ -1,9 +1,9 @@
-import { DateFilterType, DummyEventType } from "./types";
+import { DateFilterType, EventType } from "./types";
 import { FIREBASE_DEFAULT_API, EVENTS } from './endpoints'
 import axios from 'axios'
 
-export const getAllEvents = async (): Promise<DummyEventType[] | any[]> => {
-  const events: DummyEventType[] | any[] = []
+export const getAllEvents = async (): Promise<EventType[] | any[]> => {
+  const events: EventType[] | any[] = []
 
   try {
     const response = await axios.get(`${FIREBASE_DEFAULT_API}/${EVENTS}`)
@@ -28,19 +28,21 @@ export const getAllEvents = async (): Promise<DummyEventType[] | any[]> => {
   return events
 }
 
-export const getFeaturedEvents = async (): Promise<DummyEventType[] | any[]> => {
+export const getFeaturedEvents = async (): Promise<EventType[] | any[]> => {
     const events = await getAllEvents()
     
-    if (events) return events.filter((event: DummyEventType) => event.isFeatured);
+    if (events) return events.filter((event: EventType) => event.isFeatured);
 
     return events
 }
 
 
-export function getFilteredEvents(dateFilter: DateFilterType, data: DummyEventType[]) {
+export const getFilteredEvents = async (dateFilter: DateFilterType): Promise<EventType[]> => {
   const { year, month } = dateFilter;
 
-  let filteredEvents = data.filter((event) => {
+  const events = await getAllEvents()
+
+  let filteredEvents = events.filter((event) => {
     const eventDate = new Date(event.date);
     return (
       eventDate.getFullYear() === year && eventDate.getMonth() === month - 1
@@ -49,7 +51,7 @@ export function getFilteredEvents(dateFilter: DateFilterType, data: DummyEventTy
   return filteredEvents;
 }
 
-export const getEventById = async (id: string | string[] | undefined): Promise<DummyEventType | undefined> => {
+export const getEventById = async (id: string | string[] | undefined): Promise<EventType | undefined> => {
   if(typeof id === 'string') {
     const events = await getAllEvents()
     if(events) return events.find((event) => event.id === id);
@@ -57,7 +59,7 @@ export const getEventById = async (id: string | string[] | undefined): Promise<D
   return;
 }
 
-export const humanReadableDate = (date: string, withDay: boolean = true) => {
+export const humanReadableDate = (date: string, withDay: boolean = true): string => {
   if(withDay) {
     return new Date(date).toLocaleDateString("en-US", {
       day: "numeric",
@@ -72,7 +74,7 @@ export const humanReadableDate = (date: string, withDay: boolean = true) => {
   }
 }
 
-export const formattedAddress = (address: string) =>
+export const formattedAddress = (address: string): string =>
   address.replace(", ", "\n");
 
-export const fetcher = (...args: any) => fetch(args).then(res => res.json())
+export const fetcher = (...args: any): Promise<any> => fetch(args).then(res => res.json())
