1109832559c709bf0d57cb5c754561b78d477ea1
Andrea Solfanelli <andrea.solfanelli@webranking.it>
2022-12-06 14:22:43 +0100

    add _document file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { Layout } from "../components/templates";
@@ -6,6 +7,11 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
+      <Head>
+        <title>Next Events</title>
+        <meta name='description' content='NextJS Events' />
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+      </Head>
       <Component {...pageProps} />
     </Layout>
   );
